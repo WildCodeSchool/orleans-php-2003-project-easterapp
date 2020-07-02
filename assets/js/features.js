54d611b5947d3796e128f8228e54c3e2b2694868
Benoit <benoit.chocot@gmail.com>
2020-07-02 13:56:20 +0200

    fix travis

--- assets/js/features.js
+++ assets/js/features.js
@@ -1,21 +1,25 @@
 function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("sortFeature");
+    let table; let rows; let switching; let i; let x; let y; let shouldSwitch; let dir; let
+        switchcount = 0;
+    // eslint-disable-next-line prefer-const
+    table = document.getElementById('sortFeature');
     switching = true;
-    dir = "asc";
+    dir = 'asc';
     while (switching) {
         switching = false;
+        // eslint-disable-next-line prefer-destructuring
         rows = table.rows;
+        // eslint-disable-next-line no-plusplus
         for (i = 1; i < (rows.length - 1); i++) {
             shouldSwitch = false;
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            if (dir == "asc") {
+            x = rows[i].getElementsByTagName('TD')[n];
+            y = rows[i + 1].getElementsByTagName('TD')[n];
+            if (dir === 'asc') {
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                     shouldSwitch = true;
                     break;
                 }
-            } else if (dir == 'desc') {
+            } else if (dir === 'desc') {
                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
@@ -26,21 +30,20 @@ function sortTable(n) {
         if (shouldSwitch) {
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
+            // eslint-disable-next-line no-plusplus
             switchcount++;
-        } else {
-            if (switchcount === 0 && dir === "asc") {
-                dir = "desc";
-                switching = true;
-            }
+        } else if (switchcount === 0 && dir === 'asc') {
+            dir = 'desc';
+            switching = true;
         }
     }
 }
 
-window.addEventListener('load', function () {
-    document.getElementById("nameSort").addEventListener("click", function () {
+window.addEventListener('load', () => {
+    document.getElementById('nameSort').addEventListener('click', () => {
         sortTable(0);
     });
-    document.getElementById("categorySort").addEventListener("click", function () {
+    document.getElementById('categorySort').addEventListener('click', () => {
         sortTable(2);
     });
 });
